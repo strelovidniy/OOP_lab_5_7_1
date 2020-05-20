162db78108825ea49ac62d260248289202f4525b
Роман Данилевич <dwordchanel@gmail.com>
2020-05-20 14:49:17 +0300

    Add project files.

---
+++ OOP_lab_5_7_1/Doctor.cs
@@ -0,0 +1,32 @@
+﻿namespace OOP_lab_5_7_1
+{
+    class Doctor
+    {
+        private string _surename;
+        private string _profession;
+
+        public string Surename
+        {
+            get => _surename;
+            set => _surename = value;
+        }
+
+        public string Profession
+        {
+            get => _profession;
+            set => _profession = value;
+        }
+
+        public Doctor()
+        {
+            _surename = "Не вказано.";
+            _profession = "Не вказано.";
+        }
+
+        public Doctor(string surename, string profession)
+        {
+            Surename = surename;
+            Profession = profession;
+        }
+    }
+}
